e5014b1881ca044f1bfb6a5cfddb688215009b9d
Xteam <jstivenpulgarin@gmail.com>
2017-09-18 21:45:04 -0500

    Adelanto Listas Dobles

--- src/estructurasdedatos/cap2/listas/ListaDoble.java
+++ src/estructurasdedatos/cap2/listas/ListaDoble.java
@@ -5,10 +5,69 @@
  */
 package estructurasdedatos.cap2.listas;
 
+import estructurasdedatos.utiles.nodos.NodoDoble;
+
 /**
  *
  * @author tusk
  */
 public class ListaDoble {
+
+    private NodoDoble head;
+    private NodoDoble tail;
     
-}
+    //Constructor
+    public void ListaDoble() {
+        this.head = null;
+        this.tail = null;
+        
+
+    }
+
+    public void addend(int dato) {
+        NodoDoble nodo = new NodoDoble();
+        nodo.setDato(dato);
+        if (head.getSiguiente() == null) {
+
+            head = nodo;
+        } else {
+            NodoDoble temp = head;
+            while (temp.getSiguiente() != null) {
+                temp = temp.getSiguiente();
+            }
+            temp.getSiguiente(nodo);
+            nodo.getAnterior(temp);
+
+        }
+    }
+    
+    public void listar() {
+
+        if (head == null) {
+
+            NodoDoble temp = head;
+
+            int i = 0;
+
+            while (temp != null) {
+
+            System.out.print(i + ".[ " + temp.getValor() + " ]" + " ->  ");
+
+            temp = temp.getSiguiente();
+
+            i++;
+         }
+       }
+    }
+   public NodoDoble search(int dato){
+        NodoDoble temp = head;
+        while(temp.getDato()!=dato && temp != null){
+            temp=temp.getSiguiente();
+        }
+        if(temp != null){
+            return temp;
+        }else{
+            return null;
+        }
+        
+    }}
--- src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -10,5 +10,62 @@ package estructurasdedatos.utiles.nodos;
  * @author tusk
  */
 public class NodoDoble {
-    
+
+    private Integer dato;
+    private NodoDoble siguiente;
+    private NodoDoble anterior;
+
+    /**
+     * @return the dato
+     */
+    public Integer getDato() {
+        return dato;
+    }
+
+    /**
+     * @param dato the dato to set
+     */
+    public void setDato(Integer dato) {
+        this.dato = dato;
+    }
+
+    /**
+     * @return the siguiente
+     */
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+
+    /**
+     * @param siguiente the siguiente to set
+     */
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    /**
+     * @return the anterior
+     */
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+
+    /**
+     * @param anterior the anterior to set
+     */
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+
+    public void getSiguiente(NodoDoble nodo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void getAnterior(NodoDoble temp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getValor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
